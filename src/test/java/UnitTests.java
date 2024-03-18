2718faa2a7a5c1b4eff60f1ebc1e2f53bf349299
gchursov <topsycreed@gmail.com>
2024-03-18 18:29:58 +0100

    Initial commit

---
+++ src/test/java/UnitTests.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UnitTests {
+
+    @Test
+    void simpleJunitTest() {
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+
+        Assertions.assertEquals(5, sum);
+    }
+}
